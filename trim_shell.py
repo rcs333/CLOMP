e17c0b94a71ade682f6b7f4489cb8dc3139b017a
Ryan C Shean <ryanshean1@gmail.com>
2019-04-10 13:09:05 -0700

    Update trim_shell.py

--- trim_shell.py
+++ trim_shell.py
@@ -1,12 +1,28 @@
-# trim_shell.py
-# script to adapter and quality trim reads 
+#	trim_shell.py V 1.0
+#	Copyright (C) 2019 Ryan C. Shean 
+#		This program is free software: you can redistribute it and/or modify
+#		it under the terms of the GNU General Public License as published by
+#		the Free Software Foundation, either version 3 of the License, or
+#		(at your option) any later version.
+
+#		This program is distributed in the hope that it will be useful,
+#		but WITHOUT ANY WARRANTY; without even the implied warranty of
+#		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#		GNU General Public License for more details.
+
+#		You should have received a copy of the GNU General Public License
+#		along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
 
 import subprocess
 import glob
 
 # location of your Trimmomatic jar
 TRIM_LOC = '/tools/Trimmomatic-0.38/trimmomatic-0.38.jar'
-# location of your adapters file 
+# location of your adapters file I recommend using the one included in the GitHub repo
+# but if you know what you want to remove you can just substitute a custom file here
 ADAPTER_LOC = '/tools/Trimmomatic-0.38/adapters/adapters.fa'
 
 for file_name in glob.glob('*.fastq'):
