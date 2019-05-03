c9a3a5e229d498acd01acb4b8b7fa2dc2297b9a8
Ryan C Shean <ryanshean1@gmail.com>
2019-05-02 17:27:26 -0700

    Update CLOMP.py

--- CLOMP.py
+++ CLOMP.py
@@ -1,4 +1,8 @@
 #	CLOMP - Clinically Okay Metagenomic Pipeline
+# 		CLOMP is a fully functional reads to classification metagenomics classifier built on the 
+#		Excellent open source tools SNAP, bowtie2, Trimmomaticm, and Pavian 
+# 		To install and run CLOMP check out the github readme 
+
 #	Copyright (C) 2019 Ryan C. Shean 
 #		This program is free software: you can redistribute it and/or modify
 #		it under the terms of the GNU General Public License as published by
@@ -26,10 +30,11 @@ ncbi = NCBITaxa()
 
 # takes a path to the ini file and reads in all the variables, all variables
 # are globals and assumed to exist and be valid 
-# If the variable is assigned to <blank>, we are not sure what would happen.
+# Incorrectly formatted ini file would produce unspecified behavior 
 # It would be good to have a validity checker for each of these variables.  #nicetohave
 def parse_ini(ini_path):
 	for line in open(ini_path):
+		# if the line exists, is not a comment, and has a declaration try to extract one of the variables out 
 		if line[0] and line[0] != '#' and '=' in line:
 			line_list = line.split('=')
 			var = line_list[0].strip()
@@ -37,6 +42,7 @@ def parse_ini(ini_path):
 			#Of note, all of the paired-end functionality is untested.  Usually only R1 is used in taxonomical analysis.
 			if var == 'PAIRED_END':
 				global PAIRED_END
+				# ast.literal_eval reads text as python variables, this is expected to be either 'True' or 'False' so PAIRED_END will be a boolean 
 				PAIRED_END = ast.literal_eval(val)
 			elif var == 'R1':
 				global R1
@@ -89,6 +95,15 @@ def parse_ini(ini_path):
 			elif var == 'BWT_CLEAN':
 				global BWT_CLEAN
 				BWT_CLEAN = ast.literal_eval(val)
+			elif var == 'SECOND_PASS':
+				global SECOND_PASS
+				SECOND_PASS = ast.literal_eval(val)
+			elif var == 'BWT_SECOND_PASS_OPTIONS':
+				global BWT_SECOND_PASS_OPTIONS
+				BWT_SECOND_PASS_OPTIONS = val
+			elif var == 'BWT_SECOND_SUFFIX':
+				global BWT_SECOND_SUFFIX
+				BWT_SECOND_SUFFIX = val 
 			elif var == 'DB_LIST':
 				global DB_LIST
 				DB_LIST = ast.literal_eval(val)
@@ -149,7 +164,8 @@ def parse_ini(ini_path):
 				
 			
 # Takes a list of files to host filter and if output files don't exist, host filters and writes output
-# Returns a set containing all output files 
+# Returns a set containing all output files that exist even if they weren't created during this execution
+# host filtered file is also written to disk with BASE + BASE_DELIMITER + BWT_SUFFIX as the file name
 def host_filter(to_host_filter_list):
 	# Use a set to prevent duplicates when using paired end reads 
 	done_host_filtering_list = set()
@@ -204,6 +220,80 @@ def host_filter(to_host_filter_list):
 				base + BASE_DELIMITER + R2 + BWT_SUFFIX
 				subprocess.call(r2_cmd, shell=True)
 			
+			# Clean up the intermediate files 
+			if BWT_CLEAN:
+				subprocess.call('rm ' + base + '_mappedSam', shell=True)
+				subprocess.call('rm ' + base + '_mappedBam', shell=True)
+				subprocess.call('rm ' + base + '_sorted.bam', shell=True)
+				subprocess.call('rm ' + base + '_sorted.bam.bai', shell=True)
+			
+			# delete the input 
+			if not SAVE_MIDDLE_AND_INPUT_FILES:
+				subprocess.call('rm ' + r1, shell=True)
+				if PAIRED_END:
+					subprocess.call('rm ' + r2, shell=True)
+		# if the output file already exists add it to the list to send to the next step 
+		else:
+			done_host_filtering_list.add(base + BASE_DELIMITER + R1 + BWT_SUFFIX)
+			
+	return done_host_filtering_list
+
+# Another host filter function that uses the second pass variables instead of passing them at runtime - exactly identical to the host_filter() function but references different
+# globals from the ini file - this allows us to filter a large amount immediately then perform a second more sensitive pass. 	
+def host_filter2(to_host_filter_list):
+	# Use a set to prevent duplicates when using paired end reads 
+	done_host_filtering_list = set()
+	
+	# Go through every file in the file list that was passed to host_filter and take the unique sample name.
+	for r1 in to_host_filter_list:
+		base = r1.split(BASE_DELIMITER)[0]
+		
+		# Check if output file does not exist or if overwrite is turned on
+		if not os.path.isfile(base + BASE_DELIMITER + R1 + BWT_SECOND_SUFFIX) or OVERWRITE:
+			if PAIRED_END:
+				#Build name of the R2 file by taking sample name from R1 and putting in R2 where the R1 was.
+				r2 = r1.split(R1)[0] + R2 + r1.split(R1)[1]
+				
+				#Create alignment command for paired-end reads for host filtering with bowtie2.
+				align_cmd = 'bowtie2 ' + BWT_SECOND_PASS_OPTIONS + ' --threads ' + THREADS + ' -x ' + \
+					BWT_DB_LOCATION + ' -q -1 ' + r1 + + ' -2 ' + r2 + ' -S ' + base + \
+					'_mappedSam ' + ' 2>&1 | tee -a ' + base + '.log'
+			else:
+				#If not paired-end, then create alignment command for single-end reads for host filtering with bowtie2.
+				align_cmd = 'bowtie2 ' + BWT_SECOND_PASS_OPTIONS + ' --threads ' + THREADS + ' -x ' + \
+				BWT_DB_LOCATION + ' -q -U ' + r1 + ' -S ' + base + '_mappedSam' + ' 2>&1 | tee -a ' + \
+				base + '.log'
+			
+			#Call the bowtie2 command from above.
+			subprocess.call(align_cmd, shell=True)
+			
+			#Convert from sam to bam.
+			sort_cmd = 'samtools view -Sb -@ ' + THREADS + ' ' + base + '_mappedSam > ' + base + '_mappedBam' 
+			subprocess.call(sort_cmd, shell=True)
+			#Unclear if this command is needed.
+			sort_cmd2 = 'samtools sort -@ ' + THREADS + ' ' + base + '_mappedBam ' + base +  '_sorted'
+			subprocess.call(sort_cmd2, shell=True)
+			#Unclear if this command is needed.
+			index_cmd = 'samtools index ' + base + '_sorted.bam'
+			subprocess.call(index_cmd, shell=True)
+			
+			
+			#Write the output FASTQ file that has been host filtered.
+			r1_cmd = 'samtools view -@ ' + THREADS + ' -F 0x40 ' + base + '_mappedBam | ' \
+				'awk \'{if($3 == \"*\") print \"@\" $1 \"\\n\" $10 \"\\n\" \"+\" $1 \"\\n\" $11}\' > ' + \
+				base + BASE_DELIMITER + R1 + BWT_SECOND_SUFFIX
+			subprocess.call(r1_cmd,shell=True)
+			
+			#Add the output file to our "done" file list.
+			done_host_filtering_list.add(base + R1 + BWT_SECOND_SUFFIX)
+			
+			#Perform host filtering on R2 as needed and write output FASTQ that has been host filtered.
+			if PAIRED_END:
+				r2_cmd = 'samtools view -@ ' + THREADS + ' -f 0x40 ' + base + '_mappedBam | ' \
+				'awk \'{if($3 == \"*\") print \"@\" $1 \"\\n\" $10 \"\\n\" \"+\" $1 \"\\n\" $11}\' > ' + \
+				base + BASE_DELIMITER + R2 + BWT_SECOND_SUFFIX
+				subprocess.call(r2_cmd, shell=True)
+			
 			if BWT_CLEAN:
 				subprocess.call('rm ' + base + '_mappedSam', shell=True)
 				subprocess.call('rm ' + base + '_mappedBam', shell=True)
@@ -214,11 +304,9 @@ def host_filter(to_host_filter_list):
 				if PAIRED_END:
 					subprocess.call('rm ' + r2, shell=True)
 		else:
-			done_host_filtering_list.add(base + BASE_DELIMITER + R1 + BWT_SUFFIX)
+			done_host_filtering_list.add(base + BASE_DELIMITER + R1 + BWT_SECOND_SUFFIX)
 			
-	return done_host_filtering_list
-				
-		
+	return done_host_filtering_list		
 		
 # takes a string matching the end of file names that we want to trim. Uses trimmomatic to clip
 # adapter sequences and short/low quality reads. Writes output to TRIM_SUFFIX returns a set containing
@@ -226,11 +314,14 @@ def host_filter(to_host_filter_list):
 def trim(to_trim_list):
 	done_trim_list = set()
 	
+	
 	for file_name in to_trim_list:
 		base = file_name.split(BASE_DELIMITER)[0]
+		# if the output file doesn't exist yet or we want to overwrite it anyways 
 		if not os.path.isfile(base + BASE_DELIMITER + R1 + TRIM_SUFFIX) or OVERWRITE:
 			if PAIRED_END:
 				r1 = file_name
+				# this assumes that r1 and r2 have their R1 and R2 specifiers in identical places in their names and are otherwise identical 
 				r2 = file_name.split(R1)[0] + R2 + file_name.split(R2)[1]
 				trim_cmd = 'java -jar ' + TRIMMOMATIC_JAR_PATH + ' PE -threads ' + THREADS + ' ' + r1 + ' ' + \
 					r2 + ' ' + base + BASE_DELIMITER + R1 + TRIM_SUFFIX + ' ' + base + BASE_DELIMITER + R2 + TRIM_SUFFIX + ' ' + SEQUENCER + \
@@ -243,6 +334,7 @@ def trim(to_trim_list):
 				
 			subprocess.call(trim_cmd,shell=True)
 			done_trim_list.add(base + BASE_DELIMITER + R1 + TRIM_SUFFIX)
+			# delete the input 
 			if not SAVE_MIDDLE_AND_INPUT_FILES:
 				if PAIRED_END:
 					subprocess.call('rm ' + r1, shell=True)
@@ -259,18 +351,21 @@ def trim(to_trim_list):
 def snap(to_snap_list):
 	done_snap_list = set()
 	
-	# Incremental count for each database that is passed in the initialization file.  This count is appended to the output file.
+	# Incremental count for each database that is passed in the initialization file.  This count is appended to the output file name 
+	# so snap files are named by number not by database name 
 	count = -1 
-	#Loop through the databases from the initialization file.  The goal is to perform each database load into RAM only once.
+	#Loop through the databases from the initialization file. This allows us to only load each database into RAM once 
 	for db in DB_LIST:
-		#The added variable holds whether a given database has been aligned to for a set of reads.
+		# do we need to create output for this database yet? 
 		added = False 
 		count +=  1
 		
 		# Build SNAP command.  It will be long and comma-separated in order to load each database only once
-		snap_cmd = SNAP_ALIGNER_LOCTION + ' ' 
 		# if you comma separate commands all linking the same database SNAP will load the database
-		# once which DRASTICALLY speeds up the whole process
+		# once which DRASTICALLY speeds up the whole process. This also clears the previous databases command and resets 
+		snap_cmd = SNAP_ALIGNER_LOCTION + ' ' 
+		
+		# add each file that doesn't yet exist (or if overwriting all files anyways) to the SNAP command 
 		for file_name in to_snap_list:
 			base = file_name.split(BASE_DELIMITER)[0]
 			if not os.path.isfile(base + BASE_DELIMITER + str(count) + '.sam') or OVERWRITE:
@@ -287,6 +382,7 @@ def snap(to_snap_list):
 					snap_cmd += ' single ' + db + ' ' + file_name + ' -o ' + base + BASE_DELIMITER + str(count) + \
 						'.sam -t ' + THREADS + ' ' + SNAP_OPTIONS + ' , '
 			done_snap_list.add(base + BASE_DELIMITER + str(count) + '.sam')
+			
 		# only call snap if we've found at least one file without output
 		if added:
 			#Execute SNAP command built from above.
@@ -420,29 +516,41 @@ def new_write_kraken(basename, final_counts_map, num_unassigned):
 	subprocess.call(kraken_report_cmd, shell=True)
 	
 
+# takes a list of finished output files and builds sam files for species level assignments 
+# for each sample sam files are wrote directly to disk 
 def build_sams(input_list):
+	
+	# only try to import this if we're trying to build sam files 
 	from Bio import Entrez 
 	Entrez.email = ENTREZ_EMAIL
+	
+	# go through each file report output files 
 	for file_name in glob.glob('*report.tsv'):
 		base = file_name.split(BASE_DELIMITER)[0]
 		taxid_to_assemble = []
 		
+		# grab a list of taxids that are at a species level and also have greater than MIN_READ_CUTOFF assigned to them 
 		for line in open(file_name):
 			line_list = line.split('\t')
 			if line_list[3] == 'S' and int(line_lsit[2]) >= MIN_READ_CUTOFF:
 				lineage = ncbi.get_lineage(line_list[4])
+				# filter out any taxids that have lineages that include anything from the blacklist
 				if not any(x in SAM_NO_BUILD_LIST for x in lineage):
 					taxid_to_assemble.append(line_list[4])
-					
+		
+		# go through each taxid that we pulled in the last loop and parse the sam file for the accession numbers of the entries that each read aligned to
+		# Each read can align to more than one NT entry across all SNAP databases so we grab every accession number that is this taxid 
 		for taxid in taxid_to_assemble:
 			taxid_search_list = [str(taxid)]
 			taxid_search_list = taxid_search_list + ncbi.get_descendant_taxa(taxid, intermediate_nodes=True)
 			list_of_reads_to_pull = []
+			# this gets a list of every read that was assigned a given taxid 
 			for a_line in open(base + '_assignments.txt'):
 				a_line_list = a_line.split('\t')
 				if a_line_list[1]  in taxid_search_list:
 					list_of_reads_to_pull.append(a_line_list[0])
 			acc_num_list = []
+			# this gets us all accession numbers that all these reads aligned to 
 			for s_file in glob.glob(base + '*' + '.sam'):
 				for line in open(s_file):
 					sam_line_list = sam_line.split('\t')
@@ -452,15 +560,18 @@ def build_sams(input_list):
 				print('No complete genome reference found, not assembling taxid: ' + str(taxid))
 				break
 				
+			# now we figure out the most common accession number that was assigned to this taxid
 			most_common_acc_num = max(set(acc_num_list), key = acc_num_list.count)
 			
 			taxid_lineage = ncbi.get_lineage(taxid)
+			# we walk up the taxonomy tree ASSEMBLY_NODE_OFFSET nodes and pull all reads that were taxonomically assigned at or below that node
 			taxid_to_pull = taxid_lineage[ASSEMBLY_NODE_OFFSET]
 			taxid_search_list = taxid_to_pull + ncbi.get_dsecendant_taxa(taxid_to_pull, intermediate_nodes = True)
 			
 			header_list = []
 			seq_list = []
 			g = open(base + '_' + taxid + '.fasta', 'w')
+			# then we write all the reads that are at or below the taxid_to_pull variable and write them into a fasta file 
 			for line in open(base + '_assignments.txt'):
 				line_list = line.split('\t')
 				if int(line_list[1]) in taxid_search_list:
@@ -468,6 +579,7 @@ def build_sams(input_list):
 					g.write(line_list[2])
 			g.close()
 			
+			# now we download the reference fasta file for the most common acession number 
 			print('Searching NCBI for Accession number:' + most_common_acc_num + ' for taxid ' + str(taxid))
 			record = Entrez.read(Entrez.esearch(db='nucleotide', term=most_common_acc_num))
 			try:
@@ -476,14 +588,17 @@ def build_sams(input_list):
 				print(str(taxid) + ' did not return hits - not assembling')
 				break
 			
+			# build some file names for the bowtie index and our reference fasta 
 			ref_fasta = base + '_' + str(taxid) + '_ref.fasta'
 			ref_db = base + '_' + str(taxid) + '_bwt_db'
 			g = open(ref_fasta, 'w')
 			g.write(h2.read())
 			g.close()
 			print('building bowtie2 index') 
+			# build the bowtie2 index 
 			subprocess.call('bowtie2-build ' + ref_fasta + ' ' + ref_db + ' > /dev/null 2>&1 ', shell=True)
 			print('Done with index build. Aligning...')
+			# aling and output the sam file 
 			subprocess.call('bowtie2 -x ' + ref_db + ' -@ ' + THREADS + ' -f -U ' + base + '_' + str(taxid) + '.fasta --no-unal > ' + base + '_' + str(taxid) + '.sam', shell=True)
 			subprocess.call('rm ' + ref_db, shell=True)
 				
@@ -523,6 +638,9 @@ if __name__ == '__main__':
 		trim_list = trim(input_list)
 		hf_list = host_filter(trim_list)
 		to_snap_list = hf_list 
+		
+	if SECOND_PASS:
+		to_snap_list = host_filter2(to_snap_list)
 		
 	sam_list = snap(to_snap_list)
 		
@@ -624,7 +742,6 @@ if __name__ == '__main__':
 					else:
 						taxid_to_read_set[str(tax_assignment)] = set([loaded_read])
 					
-			
 				if tax_assignment in final_assignment_counts:
 					final_assignment_counts[tax_assignment] += 1
 				else:
@@ -634,6 +751,24 @@ if __name__ == '__main__':
 		e.close()
 		if BLAST_CHECK:
 			z.close()
+			subprocess.call('blastn -db ' + BLAST_CHECK_DB + ' -task blastn -query $f -num_threads 20 -evalue ' + BLAST_EVAL + ' -outfmt "6 qseqid" -max_target_seqs 1 -max_hsps 1 > blast_check.txt', shell=True)
+			redo_taxid_list = []
+			for line in open('blast_check.txt'):
+				redo_taxid_list.append(line.split())
+			n = open('new_assignments.txt', 'w')
+			for line in open(file_base + '_assignments.txt'):
+				ll = line.split('\t')
+				if ll[0] in redo_taxid_list:
+					n.write(ll[0] + '\t' + DB_TAXID + '\t' + ll[2].strip() + '\n')
+				else:
+					n.write(line)
+			n.close()
+			for item in redo_taxid_list:
+				final_assignment_counts[read_to_taxids_map[item] += 1
+				final_assignment_counts[DB_TAXID] += 1
+			
+				
+				
 	
 		#For each sample, we make a folder and for every taxid, we create a FASTA file that are named by their taxid.  We lose the read ID in this file.  #nicetohave would be hold the read ID here.
 		#Here we will write a FASTA of unique reads
@@ -656,3 +791,6 @@ if __name__ == '__main__':
 	if BUILD_SAMS:
 		build_sams(sam_list)
 	
+		
+	
+		
